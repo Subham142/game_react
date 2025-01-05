237198369f04eb7c68d759ee5e7316c4ecb0478d
subham <subhamy142@gmail.com>
2025-01-05 17:50:15 +0530

    Added heading and language chips

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from "react"
+import { languages } from "./Language"
+console.log(languages)
 
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
- */
+export default function AssemblyEndgame() {
+   
+  const languagechips = languages.map((language) => {
+      const style = {
+        backgroundColor: language.backgroundColor,
+        color : language.color
+      }
+      return(
+        <span
+        className="chip"
+        style={style}
+        key={language.name}>
+          {language.name}
+        </span>
+      )
+    })
 
-export default function Hangman() {
     return (
         <main>
-            Game goes here
+           <header>
+           <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the 
+                programming world safe from Assembly!</p>
+                <section className="game-status">
+                    <h2>You win!</h2>
+                    <p>Well done! 🎉</p>
+                </section>
+                <section className="language-chips">
+                      {languagechips}
+                </section>
+           </header>
         </main>
     )
 }
